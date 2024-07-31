a9942e882fc37811465c9dee0e00d68d2c5857b3
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-01 00:50:26 +0300

    Added Logo component

---
+++ web-project-2/src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import styles from './Logo.module.css'
+
+function Logo() {
+    return (
+        <Link>
+            <img src="" alt="" className="" />
+        </Link>
+    )
+}
+
+export default Logo
