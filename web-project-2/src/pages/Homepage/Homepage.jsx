fac4031046903aa34be05ea72927a5a11f2159dc
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-01 14:03:54 +0300

    Added stilization for the NavLinks

--- web-project-2/src/pages/Homepage/Homepage.jsx
+++ web-project-2/src/pages/Homepage/Homepage.jsx
@@ -1,18 +1,19 @@
 import styles from './Homepage.module.css'
 import { NavLink } from 'react-router-dom'
 
+
 function Homepage() {
     return (
         <div className={styles.homepage}>
         <section>
         <NavLink to='/training' >
-        Go to training
+        <img src='/athlete.jpg' alt='Go to training picture' className={styles.links} />
         </NavLink>
         <NavLink to='/nutrition' >
-        Go to nutriton
+        <img src='/nutrition.webp' alt='Go to nutriton picture' className={styles.links} />
         </NavLink>
         <NavLink to='/supplements' >
-        Go to supplements
+        <img src='/supplements.png' alt='Go to supplements picture' className={styles.links} />
         </NavLink>
         </section>
         </div>
