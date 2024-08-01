e503ade34b30e9a96491c74f8d06ff7a44f37f2f
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-01 12:58:19 +0300

    Added NavLinks and little CSS for the Homepage

--- web-project-2/src/pages/Homepage/Homepage.jsx
+++ web-project-2/src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,20 @@
 import styles from './Homepage.module.css'
+import { NavLink } from 'react-router-dom'
 
 function Homepage() {
     return (
-        <div>
-            Hello
+        <div className={styles.homepage}>
+        <section>
+        <NavLink to='/training' >
+        Go to training
+        </NavLink>
+        <NavLink to='/nutrition' >
+        Go to nutriton
+        </NavLink>
+        <NavLink to='/supplements' >
+        Go to supplements
+        </NavLink>
+        </section>
         </div>
     )
 }
