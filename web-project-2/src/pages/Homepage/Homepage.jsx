0a0da19690da54868610665ac59b508fd8d7d1fd
Ivaylo Georgiev <ivailo1411@gmail.com>
2024-08-01 20:51:41 +0300

    Fixes on file names

--- web-project-2/src/pages/Homepage/Homepage.jsx
+++ web-project-2/src/pages/Homepage/Homepage.jsx
@@ -1,35 +1,31 @@
-import styles from './Homepage.module.css'
-import { NavLink,useNavigate } from 'react-router-dom'
+import './Homepage.css';
+import { NavLink, useNavigate } from 'react-router-dom'
 import { AppContext } from '../../context/authContext';
 import { useContext } from 'react';
 
 
 function Homepage() {
-    const { user, userData, setAppState } = useContext(AppContext);
+    const { user } = useContext(AppContext);
     const navigate = useNavigate();
 
     const handleLinkClick = (e) => {
         if (!user) {
-            e.preventDefault(); 
-            navigate('/login'); 
+            e.preventDefault();
+            navigate('/login');
         }
     };
 
     return (
-        <div className={styles.homepage}>
-        <section>
-        
-        <NavLink to='/training' onClick={handleLinkClick}>
-        <img src='/athlete.jpg' alt='Go to training picture' className={styles.links} />
-        </NavLink>
-        <NavLink to='/nutrition' onClick={handleLinkClick}>
-        <img src='/nutrition.webp' alt='Go to nutriton picture' className={styles.links} />
-        </NavLink>
-        <NavLink to='/supplements' onClick={handleLinkClick}>
-        <img src='/supplements.png' alt='Go to supplements picture' className={styles.links} />
-        </NavLink>
-        
-        </section>
+        <div className="homepage">
+            <NavLink to='/training' onClick={handleLinkClick}>
+                <img src='/athlete.jpg' alt='Go to training picture' className="links" />
+            </NavLink>
+            <NavLink to='/nutrition' onClick={handleLinkClick}>
+                <img src='/nutrition.webp' alt='Go to nutrition picture' className="links" />
+            </NavLink>
+            <NavLink to='/supplements' onClick={handleLinkClick}>
+                <img src='/supplements.png' alt='Go to supplements picture' className="links" />
+            </NavLink>
         </div>
     )
 }
--- web-project-2/src/pages/Nutrition/Nutrition.jsx
+++ web-project-2/src/pages/Nutrition/Nutrition.jsx
@@ -1,18 +1,18 @@
-import styles from './Nutrition.module.css'
+import './Nutrition.css'
 
 function Nutrition() {
     return (
         <>
-        <div className={styles.text}>
+        <div className="text">
         <h1>Everything you need to know about Nutrition</h1>
         <h3>Here you will find information about quick and healthy recipes,
             a lot of information about superfoods and much more.
             Feel free to Post, share and like !
         </h3>
         </div>
-        <div  className={styles.section}>
-            <img src="/nutrition.webp" alt="Nutrition picture" className={styles.image}/>
-            <img src="/food-health.jpg" alt="Food picture" className={styles.image} />
+        <div  className="section">
+            <img src="/nutrition.webp" alt="Nutrition picture" className="about-image"/>
+            <img src="/food-health.jpg" alt="Food picture" className="about-image" />
 
         </div>
         </>
