d1cdcafc603480b51d037cf72dbe9982df955c60
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-01 19:21:20 +0300

    Minor changes on the Nutrition component

--- web-project-2/src/pages/Nutrition/Nutrition.jsx
+++ web-project-2/src/pages/Nutrition/Nutrition.jsx
@@ -1,8 +1,21 @@
+import styles from './Nutrition.module.css'
+
 function Nutrition() {
     return (
-        <div>
-            Nutrition
+        <>
+        <div className={styles.text}>
+        <h1>Everything you need to know about Nutrition</h1>
+        <h3>Here you will find information about quick and healthy recipes,
+            a lot of information about superfoods and much more.
+            Feel free to Post, share and like !
+        </h3>
         </div>
+        <div  className={styles.section}>
+            <img src="/nutrition.webp" alt="Nutrition picture" className={styles.image}/>
+            <img src="/food-health.jpg" alt="Food picture" className={styles.image} />
+
+        </div>
+        </>
     )
 }
 
