88821e9a30874eda8f6f68a1a0c85f67b12fda45
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-02 16:32:15 +0300

    Adjusted the Nutrition component

--- web-project-2/src/pages/Nutrition/Nutrition.jsx
+++ web-project-2/src/pages/Nutrition/Nutrition.jsx
@@ -2,21 +2,20 @@ import './Nutrition.css'
 
 function Nutrition() {
     return (
-        <>
+      <div className="nutrition-container">
+        <img src="/nutrition.webp" alt="Nutrition picture" className="image" />
+        
         <div className="text">
-        <h1>Everything you need to know about Nutrition</h1>
-        <h3>Here you will find information about quick and healthy recipes,
-            a lot of information about superfoods and much more.
-            Feel free to Post, share and like !
-        </h3>
+          <h1>Everything you need to know about Nutrition</h1>
+          <h3>Here you will find information about quick and healthy recipes, 
+              a lot of information about superfoods and much more.
+              Feel free to Post, share and like!
+          </h3>
         </div>
-        <div  className="section">
-            <img src="/nutrition.webp" alt="Nutrition picture" className="about-image"/>
-            <img src="/food-health.jpg" alt="Food picture" className="about-image" />
-
-        </div>
-        </>
-    )
-}
-
-export default Nutrition
+  
+        <img src="/food-health.jpg" alt="Food picture" className="image" />
+      </div>
+    );
+  }
+  
+  export default Nutrition;
