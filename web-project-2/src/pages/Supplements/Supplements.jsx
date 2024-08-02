10dbf3435e1fd98feda8f0354facbe65674f1df3
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-02 16:39:08 +0300

    Adjusted the Supplements component

--- web-project-2/src/pages/Supplements/Supplements.jsx
+++ web-project-2/src/pages/Supplements/Supplements.jsx
@@ -1,9 +1,21 @@
-function Supplements() {
-    return (
-        <div>
-            Supplements
-        </div>
-    )
-}
+import './Supplements.css'
 
-export default Supplements
+function Nutrition() {
+    return (
+      <div className="supplements-container">
+        <img src="/supplements.png" alt="Supplement picture" className="image" />
+        
+        <div className="text">
+          <h1>Everything you need to know about Supplements</h1>
+          <h3>Here you will find information about all the top supplements on
+              the market, also dosages, effects and a lot of studies.
+              Feel free to Post, share and like!
+          </h3>
+        </div>
+  
+        <img src="/supplements2.webp" alt="Food picture" className="image" />
+      </div>
+    );
+  }
+  
+  export default Nutrition;
